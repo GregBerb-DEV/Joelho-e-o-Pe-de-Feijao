43c94e940ef5cee357a529b04e8aec9086863ef3
Bernardo Mesko <bemesko@gmail.com>
2020-09-29 07:16:20 -0300

    Nomes de variáveis em inglês

--- Assets/Scripts/Controle.cs
+++ Assets/Scripts/Controle.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class Controle : MonoBehaviour
 {
     [SerializeField] //mostra no editor mesmo sem estar público
-    private int velocidade = 10;
+    private int _moveSpeed = 10;
     [SerializeField]
-    private int forcaDoPulo = 1250;
-    private float moveX;
-    private bool direita = true;
+    private int _jumpStrength = 1250;
+    private float _movementHorizontalDirection;
+    private bool _isGoingRight = true;
+    private Rigidbody2D _rigidbody2D;
 
     void Update()
     {
-        moveJogador();
+        MovePlayer();
     }
 
     private void LateUpdate()
     {
-        viraJogador();
+        TurnPlayer();
     }
 
-    void moveJogador()
+    void MovePlayer()
     {
-        moveX = Input.GetAxis("Horizontal");
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * velocidade,
+        _movementHorizontalDirection = Input.GetAxis("Horizontal");
+        _rigidbody2D.velocity = new Vector2(_movementHorizontalDirection * _moveSpeed,
                                                                      gameObject.GetComponent<Rigidbody2D>().velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
@@ -35,21 +36,21 @@ public class Controle : MonoBehaviour
 
     void pula()
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaDoPulo);
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * _jumpStrength);
     }
 
-    void viraJogador()
+    void TurnPlayer()
     {
-        if (moveX > 0)
+        if (_movementHorizontalDirection > 0)
         {
-            direita = true;
+            _isGoingRight = true;
         }
-        else if (moveX < 0)
+        else if (_movementHorizontalDirection < 0)
         {
-            direita = false;
+            _isGoingRight = false;
         }
         Vector2 escala = transform.localScale;
-        if ((escala.x > 0 && !direita) || (escala.x < 0 && direita))
+        if ((escala.x > 0 && !_isGoingRight) || (escala.x < 0 && _isGoingRight))
         {
             escala.x = escala.x * -1;
             transform.localScale = escala;
