7e923b0dc1038fe3d1998778c8f8a680b35b6c1d
Gregori Sabel <seriosabel57@gmail.com>
2020-10-07 18:37:42 -0300

    Projetil mata

--- Assets/Scripts/Pojectile.cs
+++ Assets/Scripts/Pojectile.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class Pojectile : MonoBehaviour
 {
 
+    [SerializeField]
+    private int damage = 5;
     public float speed;
     public float lifeTime = 1f;
     public float distance;
-    public LayerMask WhatIsSolid;
 
+    public LayerMask WhatIsSolid;
     public GameObject destroyEffect;
 
     private void Start(){
         Invoke("DestroyProjectile", lifeTime);
+
     }
     void Update()
     {
+
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, WhatIsSolid);
         if (hitInfo.collider != null){
             Debug.Log("hit collider != null");
             if (hitInfo.collider.CompareTag("inimigo")){
-                Debug.Log("Hit Bro! So Cool B)");
+                Debug.Log("compareTag == inimigo");
+                hitInfo.collider.GetComponent<EnemyHealth>().TakeDamage(damage);
+
             }
             DestroyProjectile();
         }
