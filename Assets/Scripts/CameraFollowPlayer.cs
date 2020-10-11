e045a0f68a7132e1a86233204b28c1e018e55a80
Gregori Sabel <seriosabel57@gmail.com>
2020-10-11 15:24:44 -0300

    adições do be

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -21,6 +21,6 @@ public class CameraFollowPlayer : MonoBehaviour
     {
         float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
         float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
-        transform.position = new Vector3(x + 2, y, gameObject.transform.position.z);
+        transform.position = new Vector3(x, y, gameObject.transform.position.z);
     }
 }
