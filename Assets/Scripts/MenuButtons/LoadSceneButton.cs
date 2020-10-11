a401aa8652ea3a5e6bad4686b253edcc5a7201b7
Bernardo Mesko <bemesko@gmail.com>
2020-10-11 07:56:46 -0300

    Menu overhaul

---
+++ Assets/Scripts/MenuButtons/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    [SerializeField]
+    private string _sceneToLoad;
+    public void OnClick()
+    {
+        SceneManager.LoadScene(_sceneToLoad);
+    }
+}
