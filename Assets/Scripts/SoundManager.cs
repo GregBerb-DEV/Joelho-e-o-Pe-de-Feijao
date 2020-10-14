a8fa2c67bdc093904ea5fc62d3601c016369943e
Gregori Sabel <seriosabel57@gmail.com>
2020-10-13 21:19:55 -0300

    quase sons :/

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip playerPunchSound;
+    public static AudioClip playerDamageSound;
+    public static AudioClip playerJumpSound;
+
+    static AudioSource audioSrc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPunchSound = SFX.load<>
+        //https://www.youtube.com/watch?v=8pFlnyfRfRc
+        //greg desistiu e foi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
